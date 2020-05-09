5cf8ba505ea14287fbffe10458a9a32ae1cf99db
thomas delhaye <thomas.del.haye@outlook.fr>
2020-05-09 23:08:05 +0200

    edit and show formation

--- src/main/java/com/example/demo/controllers/FormationController.java
+++ src/main/java/com/example/demo/controllers/FormationController.java
@@ -1,13 +1,54 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.HibernateUtil;
+import com.example.demo.bean.Formation;
+import com.example.demo.bean.Presentation;
 
 @Controller
 public class FormationController {
 
 	@GetMapping("/formation")
-	public String formation() {
+	public String formation(Model model, HttpSession session) {
+		
+		Boolean isAdmin = (Boolean) session.getAttribute("isAdmin?");
+		model.addAttribute("isadmin", isAdmin );
+		
+        Session session2 = HibernateUtil.getSessionFactory().openSession();      
+        session2.beginTransaction();
+        Query q = session2.createQuery("From Formation ");
+        
+        List<Formation> resultList = q.list();
+        
+        model.addAttribute("resultList", resultList);
+        
+        List<Presentation> pres = getPres();
+   	 	model.addAttribute("presName", pres.get(0).getName());
+   	 	model.addAttribute("presJob", pres.get(0).getJob());
+   	 	model.addAttribute("presEmail", pres.get(0).getEmail());
+   	 	model.addAttribute("presNumber", pres.get(0).getNumber());
+   	 	model.addAttribute("presAddress", pres.get(0).getAddress());
+   	 	model.addAttribute("presGithub", pres.get(0).getGithub());
+
+   	 	
+   	 	
 		return "pages/formation";
 	}
+	
+	public List<Presentation> getPres() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Query q = session.createQuery("From Presentation where id=1 ");
+        List<Presentation> resultList = q.list();
+        return resultList;
+	}
+	
 }
--- src/main/java/com/example/demo/controllers/FormationFormController.java
+++ src/main/java/com/example/demo/controllers/FormationFormController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,14 +25,14 @@ import com.example.demo.bean.Presentation;
 public class FormationFormController {
 	 
     @RequestMapping(value="/createFormation", method = RequestMethod.GET)
-    public String editPresForm(Model model) {
+    public String createFormation(Model model) {
         model.addAttribute("formation", new Formation());
         
     	return "form/formationForm";
     }
     
     @RequestMapping(value="/createFormation", method = RequestMethod.POST)
-    public String login(@ModelAttribute Formation formation, Model model, HttpServletRequest request,  HttpSession session) {
+    public String createFormation(@ModelAttribute Formation formation, Model model, HttpServletRequest request,  HttpSession session) {
 
     	model.addAttribute("formation", new Formation());
     	
@@ -49,8 +50,44 @@ public class FormationFormController {
     	session2.save(forma);
         session2.getTransaction().commit();
    	 	session2.close();
-        	return "redirect:/";
+        	return "redirect:/formation";
 
     }
-	
+    
+    @RequestMapping(value="/editFormation/{id}", method = RequestMethod.GET)
+    public String editFormation(Model model, @PathVariable int id) {
+        model.addAttribute("formation", new Formation());
+        model.addAttribute("id", id);
+        
+    	return "form/editFormationForm";
+    }
+
+    @RequestMapping(value="/editFormation/{id}", method = RequestMethod.POST)
+    public String editFormation(@ModelAttribute Formation formation, Model model, HttpServletRequest request,  HttpSession session, @PathVariable int id) {
+
+    	model.addAttribute("formation", new Formation());
+    	
+		Boolean isAdmin = (Boolean) session.getAttribute("isAdmin?");
+		System.out.println(isAdmin);
+        Session session2 = HibernateUtil.getSessionFactory().openSession();
+        
+ 
+        // Formation forma = new Formation(id, formation.getTitle(), formation.getDate(), formation.getSchool(), formation.getDescription());
+
+        if(isAdmin == null) {
+        	return "redirect:/login";
+        }
+    	session2.beginTransaction();
+    	Formation formaLoad = (Formation) session2.load(Formation.class, id);
+    	formaLoad.setTitle(formation.getTitle());
+    	formaLoad.setDate(formation.getDate());
+    	formaLoad.setSchool(formation.getSchool());
+    	formaLoad.setDescription(formation.getDescription());
+    	
+    	session2.update(formaLoad);
+        session2.getTransaction().commit();
+   	 	session2.close();
+        	return "redirect:/formation";
+
+    }
 }
